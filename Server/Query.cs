ca65b5fcbbec90bafbc6a1a5abdc266ddd5fa786
Neomaster <the@neomaster.net>
2022-03-10 15:21:10 +0300

    0.2.0.1

--- Server/Query.cs
+++ Server/Query.cs
@@ -6,6 +6,11 @@ public class Query
     {
         DirectoryInfo di = new DirectoryInfo(settings["fs-root"]);
 
+        if (!di.Exists)
+        {
+            return new Node[0];
+        }
+
         return di
             .GetDirectories("*", SearchOption.TopDirectoryOnly)
             .Select(d => new Node(d.FullName));
